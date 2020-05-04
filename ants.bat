7e501a87eca89fd7a9b7f5c5e534735e131bc0f8
lemy0715 <lemy0715@gmail.com>
2020-05-05 01:51:20 +0900

    docker backgroup mode구동

--- ants.bat
+++ ants.bat
@@ -1,3 +1,3 @@
 @echo off
 docker stop antsbot
-docker run --rm -v %cd%/configs:/workspaces/Ants-Auto-Trading-Bot/configs -v %cd%/ssh:/workspaces/.ssh -v %cd%/logs:/workspaces/Ants-Auto-Trading-Bot/logs --name antsbot ccassistant/antsbot
+docker run -d --rm -v %cd%/configs:/workspaces/Ants-Auto-Trading-Bot/configs -v %cd%/ssh:/workspaces/.ssh -v %cd%/logs:/workspaces/Ants-Auto-Trading-Bot/logs --name antsbot ccassistant/antsbot
--- ants.sh
+++ ants.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
 
-# echo `pwd`
 docker stop antsbot
-docker run --rm -v `pwd`/configs:/workspaces/Ants-Auto-Trading-Bot/configs -v `pwd`/ssh:/workspaces/.ssh -v `pwd`/logs:/workspaces/Ants-Auto-Trading-Bot/logs --name antsbot ccassistant/antsbot
+docker run -d --rm -v `pwd`/configs:/workspaces/Ants-Auto-Trading-Bot/configs -v `pwd`/ssh:/workspaces/.ssh -v `pwd`/logs:/workspaces/Ants-Auto-Trading-Bot/logs --name antsbot ccassistant/antsbot
